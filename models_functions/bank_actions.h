5ca29f1ffe5856752c6f59521aaefcfb212cc7f4
phmucelin <contatopedromucelin05@gmail.com>
2026-05-09 03:20:40 -0300

    feat: bank actions and bank function model

---
+++ models_functions/bank_actions.h
@@ -0,0 +1,11 @@
+#ifndef BANK_ACTIONS_H
+#define BANK_ACTIONS_H
+
+typedef struct Bank Bank;
+typedef struct Users Users;
+
+int is_user_from_bank(Bank* bank, Users* user);
+int approve_user_account(Bank* bank, Users* user);
+int reject_user_account(Bank* bank, Users* user);
+
+#endif
---
+++ services/bank_actions.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "../models/bank_model.h"
+#include "../models/user_model.h"
+
+int is_user_from_bank(Bank* bank, Users* user)
+{
+	if (!bank || !user) {
+		return 0;
+	}
+	return user->bank == bank;
+}
+
+int approve_user_account(Bank* bank, Users* user)
+{
+	if (!bank || !user) {
+		return 0;
+	}
+	if (user->bank != NULL) {
+		return 0; // Usuario ja possui banco associado
+	}
+	user->bank = bank;
+	bank->qtdClients += 1;
+	return 1;
+}
+
+int reject_user_account(Bank* bank, Users* user)
+{
+	if (!bank || !user) {
+		return 0;
+	}
+	if (user->bank != bank) {
+		return 0; // Usuario nao pertence a este banco
+	}
+	user->bank = NULL;
+	if (bank->qtdClients > 0) {
+		bank->qtdClients -= 1;
+	}
+	return 1;
+}
