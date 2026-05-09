4d79a864b6bf02183aec83c7affef0481abb5e7c
phmucelin <contatopedromucelin05@gmail.com>
2026-05-09 13:06:49 -0300

    feat: exchange service and models updated

--- models_functions/exchange.h
+++ models_functions/exchange.h
@@ -6,6 +6,6 @@ typedef struct Exchange Exchange;
 /*
  * Necessario sempre chamar verify_admin(char* uuid), antes de permitir executar essa function.
  */
-Exchange* register_exchanges(char* name, int id, int qtdClients); 
+Exchange* register_exchanges(char* name, int qtdClients); 
 
 #endif
---
+++ services/exchange.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../models/exchange_model.h"
+#include "../models_functions/exchange.h"
+#include "../services/user_actions.c"
+
+Exchange* register_exchanges(char* name, int qtdClients, char* addres, char* country)
+{
+    Exchange* new_exchange = (Exchange*)malloc(sizeof(Exchange));
+    if (new_exchange == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed for new exchange.\n");
+        return NULL;
+    }
+
+    new_exchange->name = strdup(name);
+    new_exchange->qtdClients = qtdClients;
+    new_exchange->id = generate_uuid();
+    new_exchange->addres = strdup(addres);
+    new_exchange->country = strdup(country);
+    new_exchange->coin = NULL; // vai ser definido depois, nao tem motivo para ser definido agora
+
+    return new_exchange;
+}
