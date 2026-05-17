f78357ad56235883827470a874de0b14e7a1050c
phmucelin <contatopedromucelin05@gmail.com>
2026-05-17 01:25:01 -0300

    feat: initial db commits

--- db/db.h
+++ db/db.h
@@ -1,11 +1,11 @@
 #ifndef DB_H
 #define DB_H
 
-typedef struct User User;
+typedef struct Block Block;
 typedef struct Transaction Transaction;
-
-bool save_user_infos_db(User* r);
-
+  
 bool save_transactions_info_db(Transaction* t);
+
+bool save_block_info_db(Block* t);
 
 #endif
