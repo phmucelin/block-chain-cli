211ba06b02ca1984ffeb44f339f7cc6d7bbb9f0a
phmucelin <contatopedromucelin05@gmail.com>
2026-05-17 23:08:19 -0300

    feat: created tables for transactions and blocks

--- db/db.h
+++ db/db.h
@@ -1,11 +1,21 @@
 #ifndef DB_H
 #define DB_H
 
+#include <libpq-fe.h>
+  
 typedef struct Block Block;
 typedef struct Transaction Transaction;
-  
-bool save_transactions_info_db(Transaction* t);
 
-bool save_block_info_db(Block* t);
+int try_connect_db(PGconn **conn);
+
+int disconnect_db(PGconn *conn);
+
+int create_tables_transactions_db(PGconn *conn);
+
+int create_tables_blocks_db(PGconn *conn);
+
+int save_transactions_info_db(Transaction* t);
+
+int save_block_info_db(Block* t);
 
 #endif
